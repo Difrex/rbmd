6b3b38d89f6a6ca324ffb811b907c73647bc5bd3
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2016-12-15 08:45:02 +0300

    zk

---
+++ src/rbmd/zoo.go
@@ -0,0 +1,34 @@
+package rbmd
+
+import (
+	"github.com/samuel/go-zookeeper/zk"
+	"strings"
+)
+
+type ZooNode struct {
+	Path string
+	Conn *zk.Conn
+}
+
+func (z ZooNode) EnsureZooPath(node string) (string, error) {
+	flag := int32(0)
+	acl := zk.WorldACL(zk.PermAll)
+
+	zoopath := strings.Join([]string{z.Path, "/", node}, "")
+	s := strings.Split(zoopath, "/")
+
+	var p []string
+	var fullnodepath string
+	
+	for i := 1; i < len(s); i++ {
+		p = append(p, strings.Join([]string{"/", s[i]}, ""))
+	}
+	
+	for i := 0; i < len(p); i++ {
+		fullnodepath = strings.Join([]string{fullnodepath, p[i]}, "")
+		z.Conn.Create(fullnodepath, []byte(""), flag, acl)
+	}
+	
+	return fullnodepath, nil
+}
+
